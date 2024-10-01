febe562aed48629d193164b2b263ff58d5b8b8d0
Tomilore701 <tomiloreosikoya29@gmail.com>
2024-09-30 20:15:52 -0400

    completed lab3

--- app/src/main/java/com/cst3104/androidlabs/MainActivity.java
+++ app/src/main/java/com/cst3104/androidlabs/MainActivity.java
@@ -2,10 +2,14 @@ package com.cst3104.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.Toast;
 import com.google.android.material.snackbar.Snackbar;
@@ -13,42 +17,28 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imgView;
+    Switch sw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        Button clickButton = findViewById(R.id.clickButton);
-        CheckBox checkBox = findViewById(R.id.checkBox);
-        ImageButton flagButton = findViewById(R.id.flagButton);
-        Switch switchButton = findViewById(R.id.switchButton);
-        EditText editText = findViewById(R.id.editText);
-
-
-        clickButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,
-                        getResources().getString(R.string.toast_message),
-                        Toast.LENGTH_LONG).show();
+        imgView = findViewById(R.id.flagview);
+        sw = findViewById(R.id.spin_switch);
+        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
+            if (isChecked) {
+                RotateAnimation rotate = new RotateAnimation(0, 360,
+                        Animation.RELATIVE_TO_SELF, 0.5f,
+                        Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+                imgView.startAnimation(rotate);
+            }else{
+                imgView.clearAnimation();
             }
-        });
 
 
-        switchButton.setOnCheckedChangeListener((compoundButton, isChecked) -> {
-            // Create a message for the Snackbar
-            String message = isChecked ? "Switch is now on" : "Switch is now off";
-
-
-            Snackbar.make(compoundButton, message, Snackbar.LENGTH_LONG)
-                    .setAction("Undo", new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            // Revert the switch state
-                            switchButton.setChecked(!isChecked);
-                        }
-                    }).show();
         });
     }
 }
